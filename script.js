7a1ce6100931abd0fb991f586a48035dccfa2455
mkleemoff <mkleemjr@gmail.com>
2021-03-26 10:37:36 -0400

    Create a function to generate random numbers.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Create a function to generate random numbers
+let generateRandNum(num) {
+  // Gets number from 0 -> num - 1
+  return Math.floor(Math.random() * num);
+}
+
 const randMessages = {
   messages1: [],
   messages2: [],
