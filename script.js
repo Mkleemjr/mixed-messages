ac470befe87e558252f7ae3279d9428598faaa69
mkleemoff <mkleemjr@gmail.com>
2021-03-26 10:53:08 -0400

    Add strings to all 3 messages arrays in randMessages object.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let generateRandNum(num) {
 }
 
 const randMessages = {
-  messages1: [],
-  messages2: [],
-  messages3: [],
+  messages1: ['Life is like a box of chocolates, which one did you get?', 'When you fall down, get back up again!', 'Keep learning, cause there is always more', 'Never give up!'],
+  messages2: ['Eat more fruit: have you had at least one today?', 'Vegetables are good for you!', 'Drink water, not soda!', 'Eat fresh fish like snapper and branzino properly cooked that weigh between 1 and 10 pounds'],
+  messages3: ['Exercise does a body good!', 'Take a walk after a good meal', 'Yoga is amazing: stretch those ligaments and joints!', 'If you are busy, at least do some jumping jacks in place for a few minutes!'],
 }
